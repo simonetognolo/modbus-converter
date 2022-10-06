8ffa1fc3c5e4072eca83e7cabc0a569ee956a7f0
Simone Tognolo <simone.tognolo@ima.it>
2022-10-06 10:26:50 +0200

    Add code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModbusConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TbValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EvalValues();
+        }
+
+        private void EvalValues()
+        {
+            if (TbLong == null)
+                return;
+
+            if (TbHighValue ==  null || !int.TryParse(TbHighValue.Text, out var highValue))
+                highValue = 0;
+
+            if (TbLowValue == null || !int.TryParse(TbLowValue.Text, out var lowValue))
+                lowValue = 0;
+
+            // Long result
+            var merge = unchecked(BitConverter.IsLittleEndian
+                ? (ushort)highValue << 16 | (ushort)lowValue
+                : (ushort)lowValue << 16 | (ushort)highValue);
+
+            TbLong.Text = merge.ToString();
+
+            // ASCII result
+            var lowAscii = unchecked(BitConverter.GetBytes((ushort)lowValue));
+            var highAscii = unchecked(BitConverter.GetBytes((ushort)highValue));
+
+            TbAscii.Text = System.Text.Encoding.ASCII.GetString(lowAscii) + " - " + System.Text.Encoding.ASCII.GetString(highAscii);
+        }
+    }
+}
